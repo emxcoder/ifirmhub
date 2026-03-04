45fe746a5689180addc8f7187d39ca5817d6e014
emxcoder <emxcoder@gmail.com>
2026-03-04 08:31:37 -0800

    add safe provider on image product

--- lib/pages/home_page/product_page/product_card.dart
+++ lib/pages/home_page/product_page/product_card.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ifirmhub/services/internet/network_provider.dart';
 import '../../../core/models/products_model.dart';
 
-class ProductCard extends StatelessWidget {
+class ProductCard extends ConsumerWidget {
   final Product product;
 
   const ProductCard({super.key, required this.product});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final internetCheck = ref.watch(internetStatusProvider);
     return GestureDetector(
       onTap: () {},
       child: Card(
@@ -21,16 +24,25 @@ class ProductCard extends StatelessWidget {
           children: [
             Expanded(
               child: ClipRRect(
-                borderRadius: const BorderRadius.vertical(
-                  top: Radius.circular(16),
-                ),
-                child: Image.network(
-                  semanticLabel: product.name,
-                  product.imageUrl,
-                  fit: BoxFit.cover,
-                  width: double.infinity,
-                ),
-              ),
+                  borderRadius: const BorderRadius.vertical(
+                    top: Radius.circular(16),
+                  ),
+                  child: internetCheck.when(
+                    data: (data) {
+                      return Image.network(
+                        semanticLabel: product.name,
+                        product.imageUrl,
+                        fit: BoxFit.cover,
+                        width: double.infinity,
+                      );
+                    },
+                    error: (error, stackTrace) {
+                      return Icon(Icons.devices);
+                    },
+                    loading: () {
+                      return Icon(Icons.devices);
+                    },
+                  )),
             ),
             Padding(
               padding: const EdgeInsets.all(5),
